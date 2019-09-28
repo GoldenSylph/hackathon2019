d31ed3373de65d8f927580851a3f6357750ef6df
Oleg Bedrin <numeralhuman@gmail.com>
2019-09-28 14:37:11 +0300

    Adding respawn template

---
+++ Assets/Scripts/SmartHouse/VrInteractions/RespawnPoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace SmartHouse.VrInteractions
+{
+    public class RespawnPoint : MonoBehaviour
+    {
+        public GameObject toRespawn;
+        private void Start()
+        {
+//            StartCoroutine(RespawnCoroutine());
+        }
+
+        private IEnumerator RespawnCoroutine()
+        {
+            while (true)
+            {
+//                if (toRespawn.transform.position - transform.position)
+//                {
+//                    
+//                }
+            }
+        }
+    }
+}
